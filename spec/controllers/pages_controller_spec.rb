b31f2eba84d2f660e6399696d46b5aadd3ad5f51
Rob Beattie <rob@robertbeattie.co.uk>
2011-06-04 13:24:41 +0100

    Added blueprint css

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
+
+
 describe PagesController do
   render_views
-
+  before(:each) do 
+    @base_title = "Rails Sample App | "
+  end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -10,7 +14,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Rails Sample App | Home")
+      response.should have_selector("title", :content => @base_title + "Home")
     end
   end
 
@@ -21,7 +25,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Rails Sample App | Contact")
+      response.should have_selector("title", :content => @base_title +"Contact")
     end
   end
 
@@ -33,7 +37,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Rails Sample App | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end
   
     
